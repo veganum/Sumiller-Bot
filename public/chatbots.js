c40ea936b0771134525237ceff3870fc2721539f
José <veganum@hotmail.com>
2020-09-25 11:07:17 +0200

    añadiendo eventos de js

--- public/chatbots.js
+++ public/chatbots.js
@@ -5,11 +5,21 @@ window.addEventListener('dfMessengerLoaded', function (event) {
 
 
     //EVENTOS
-    dfMessenger.renderCustomText('Buenos días');
+    dfMessenger.renderCustomText('🍷 Pulse para encontrar su vino 🥂');
 
+
+    /*
     //Manejar un evento al pulsar sobre una lista
     dfMessenger.addEventListener('df-list-element-clicked', function (event) {
-        console.log("df-list-element-clicked", event);
+        console.log("df-list-element-clicked", event.detail.element.title);
+        alert("has pulsado la opcion" + event.detail.element.title);
+    });
+    */
+
+    //Gestion de la entrada de texto
+    dfMessenger.addEventListener('df-user-input-entered', function (event) {
+        console.log("df-user-input-entered:", event.detail.input);
+
     });
 
 });
